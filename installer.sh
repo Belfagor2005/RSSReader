11c55815e27a6d1a7f1590e0e7de48c78d572f80
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-11-15 20:19:42 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 ## setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/Belfagor2005/RSSReader/main/installer.sh -O - | /bin/sh
-exec > >(tee -a /tmp/RSSReader_debug.txt) 2>&1
-set -x
+
 ## Only This 2 lines to edit with new version ######
 version='1.13'
 changelog='\nAdd Live Upgrade\nFix screen'
